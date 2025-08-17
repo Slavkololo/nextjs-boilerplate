85ca13a8f525b507e77b6cbdbcc22b378d14050f
Sviatoslav Khmelnychyi <pekachumon@gmail.com>
2025-08-18 02:30:25 +0300

    feat: change title

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
     <div>
       <main style={{ textAlign: "center" }}>
         <h2 style={{ fontSize: 20, margin: "24px auto" }}>
-          Герман поки ти на ґаматетію дрочиш я вже майже пік пройшов
+          Герман поки ця чілібамбонька на ґаматетію дрочить я вже майже пік
+          пройшов
         </h2>
         <Image
           src={german}
